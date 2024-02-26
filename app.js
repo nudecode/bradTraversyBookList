4badd718bb047c1d0e8b2eee79b49d930e93b9a6
Glen Allen <glena072@gmail.com>
2024-02-26 13:05:44 +0800

    fist commit

---
+++ app.js
@@ -0,0 +1,74 @@
+// Book Class Represents a book
+class Book {
+    constructor(title, author, isbn) {
+        this.title;
+        this.author;
+        this.isbn;
+    }
+}
+
+// UI Class Handle UI Tasks
+class UI {
+    // display books
+    static displayBooks(){
+        const StoredBooks = [
+            {
+                title: 'Harry Potter and the Philosophers Stone',
+                author: 'J.K Rowling',
+                isbn: '890123456789',
+            },
+            {
+                title: 'Star Wars A New Hope',
+                author: 'George Lucas',
+                isbn:   '890987654321'
+            }
+            
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));  
+    }
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm
+        delete"><i class="fa-solid fa-trash"></td>
+        `;
+
+        list.appendChild(row);
+
+    }
+}
+
+// Store Class Handles Storage
+
+// Event: Display books 
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => 
+{
+//  Prevent default submit
+e.preventDefault();
+//  Get form values
+const title = document.querySelector('#title').value;
+const author = document.querySelector('#author').value;
+const isbn = document.querySelector('#isbn').value;
+
+// instatiate book
+
+const book = new Book(title, author, isbn);
+console.log(book);
+// Add book to UI
+UI.addBookToList(book);
+
+});
+
+// Event: Remove
